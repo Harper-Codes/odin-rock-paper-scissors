e135ce96959cd72c170fe01157308d7d1f03f4d6
Bryan Harper <bryharper@hotmail.com>
2023-02-13 18:34:29 -0800

    Structured basic game logic.

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,33 @@
-console.log("Hi");
+let computerSelection;
+let playerSelection;
+
+const optionsArray = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+    return optionsArray[~~(Math.random() * optionsArray.length)];
+}
+
+function getPlayerChoice() {
+    let playerInput = prompt("Your move: ").toLowerCase();
+    return playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log(`You: ${playerSelection}`);
+    console.log(`AI: ${computerSelection}`);
+    
+    if (playerSelection == computerSelection) {
+        console.log("Tied Game!");
+    } else if (
+        playerSelection == "Rock" && computerSelection == "Scissors" ||
+        playerSelection == "Paper" && computerSelection == "Rock" ||
+        playerSelection == "Scissors" && computerSelection == "Paper" ){
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
+    } else {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    }
+}
+
+computerSelection = getComputerChoice();
+playerSelection = getPlayerChoice();
+console.log(playRound(playerSelection, computerSelection));
